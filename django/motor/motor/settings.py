510a71fd55ebc016f23bfa6d8399254acbb3db5b
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-05-24 12:22:31 -0300

    Ajuste arquivos estaticos em setings.py

--- django/motor/motor/settings.py
+++ django/motor/motor/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #Ambiente de produção
 #STATIC_ROOT = BASE_DIR  / 'static/'
 
